6a64a492a4613deaa05a9df504e6ccad8fb05cd5
Colin Swenson-Healey <chozandrias76@gmail.com>
2025-04-09 15:32:28 -0700

    feat: add boilerplate example scene

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3dBundle::default());
+    commands.spawn(DirectionalLightBundle::default());
+
+    // TODO: Load models, setup controls, audio, etc.
+}
